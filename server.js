961c6dcd513c019b3de4f769777a5c3c598af542
Linh Le Tuan <linh.tuan@actum.cz>
2018-04-21 08:41:18 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World! hovno'))
+
+app.post('/', (req, res) => res.send('Got a POST request'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
