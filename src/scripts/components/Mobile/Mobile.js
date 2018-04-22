dfcaf8b98a5ab0eeaea43a06cdf3eed14d13ece1
Linh Le Tuan <linh.tuan@actum.cz>
2018-04-22 09:13:26 +0200

    add number label to buttons

--- src/scripts/components/Mobile/Mobile.js
+++ src/scripts/components/Mobile/Mobile.js
@@ -19,15 +19,15 @@ import {
 
 class Mobile extends Component {
     numericButtonsDefinition = [
-        { label: '-', value: '' },
-        { label: 'abc', value: '2' },
-        { label: 'def', value: '3' },
-        { label: 'ghi', value: '4' },
-        { label: 'jkl', value: '5' },
-        { label: 'mno', value: '6' },
-        { label: 'pqrs', value: '7' },
-        { label: 'tuv', value: '8' },
-        { label: 'wxyz', value: '9' }
+        { label: '1 _', value: '' },
+        { label: '2 abc', value: '2' },
+        { label: '3 def', value: '3' },
+        { label: '4 ghi', value: '4' },
+        { label: '5 jkl', value: '5' },
+        { label: '6 mno', value: '6' },
+        { label: '7 pqrs', value: '7' },
+        { label: '8 tuv', value: '8' },
+        { label: '9 wxyz', value: '9' }
     ]
 
     handleClickSendButton = () => {
